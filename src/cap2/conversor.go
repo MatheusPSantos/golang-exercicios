03b3239c62ac00d7a079e177d9fb6f14f704bcd3
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-04 08:17:20 -0300

    initial commit

---
+++ src/cap2/conversor.go
@@ -0,0 +1,45 @@
+package main
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Use: conversor <valores> <unidade>")
+		os.Exit(1)
+	}
+
+	unidadeOrigem := os.Args[len(os.Args) - 1]
+	valoresOrigem := os.Args[1 : len(os.Args) - 1]
+
+	var unidadeDestino string
+
+	if unidadeOrigem == "cel" {
+		unidadeDestino = "fahr"
+	} else if unidadeOrigem == "km" {
+		unidadeDestino = "milhas"
+	} else {
+		fmt.Printf("%s não é uma unidade conhecida.\n", unidadeDestino)
+		os.Exit(1)
+	}
+
+	for i, v := range valoresOrigem {
+		valorOrigem, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			fmt.Printf("O valor %s na posição %d não é um número válido!\n", v, i)
+			os.Exit(1)
+		}
+
+		var valorDestino float64
+
+		if unidadeOrigem == "cel" {
+			valorDestino = valorOrigem*1.8 + 32
+		}else {
+			valorDestino = valorOrigem / 1.60934
+		}
+
+		fmt.Printf("%.2f %s = %.2f %s\n", valorOrigem, unidadeOrigem, valorDestino, unidadeDestino)
+	}
+}
