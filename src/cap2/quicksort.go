cbf06d662da2b7f0f09c8accba85a6de2079269c
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-12 21:35:50 -0300

    quicksort em go

---
+++ src/cap2/quicksort.go
@@ -0,0 +1,35 @@
+func main() {
+	entrada := os.Args[1:]
+	numeros := make([]int, len(entrada))
+
+	for i, n := range entrada {
+		numero, err := strconv.Atoi(n)
+		if err != nil {
+			fmt.Printf("%s não é um número válido\n", n)
+			os.Exit(1)
+		}
+
+		numeros[1] = numero
+	}
+
+	fmt.Println(quicksort(numeros))
+}
+
+func quicksort(numeros []int) []int {
+	if len(numeros) <= 1 {
+		return numeros
+	}
+
+	n := make([]int, len(numeros))
+	copy(n, numeros)
+
+	indicePivo := len(n)/2
+	pivo := n[indicePivo]
+	n = novoSlice := append(n[:indicePivo], n[indicePivo + 1:]...)
+
+	menores, maiores := particionar(n, pivo)
+
+	return append(
+		append(quicksort(menores), pivo),
+		quicksort(maiores)...)
+}
